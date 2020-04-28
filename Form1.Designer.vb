330016cd4d96d5ad2ddd989e8aff62a2fab981da
Gabrielle <64449210+Gabi-1403@users.noreply.github.com>
2020-04-27 23:25:58 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.valorCriterio = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.primeiraparcela = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(75, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Tipo Critério"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(75, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Valor Critério: "
+        '
+        'valorCriterio
+        '
+        Me.valorCriterio.BackColor = System.Drawing.SystemColors.Menu
+        Me.valorCriterio.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, CType(0, Byte))
+        Me.valorCriterio.Location = New System.Drawing.Point(181, 98)
+        Me.valorCriterio.Name = "valorCriterio"
+        Me.valorCriterio.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.valorCriterio.Size = New System.Drawing.Size(121, 21)
+        Me.valorCriterio.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(70, 182)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Qtde Parcelas:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Menu
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(181, 181)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(42, 26)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(21, 231)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(162, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Data Primeira Parcela"
+        '
+        'primeiraparcela
+        '
+        Me.primeiraparcela.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.primeiraparcela.Location = New System.Drawing.Point(190, 230)
+        Me.primeiraparcela.Name = "primeiraparcela"
+        Me.primeiraparcela.Size = New System.Drawing.Size(112, 20)
+        Me.primeiraparcela.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(227, 274)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 29)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Enviar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Menu
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.ForeColor = System.Drawing.Color.Red
+        Me.TextBox2.Location = New System.Drawing.Point(25, 351)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(567, 13)
+        Me.TextBox2.TabIndex = 10
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Físico", "Jurídico"})
+        Me.ComboBox1.Location = New System.Drawing.Point(174, 132)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox1.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.primeiraparcela)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.valorCriterio)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents valorCriterio As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents primeiraparcela As DateTimePicker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,169 @@
+﻿Public Class Form1
+
+    Public valorCredito As Double = 0
+    Public tipoCredito As String
+    Public parcelas As Integer = 0
+    Public dataVencimento As Date = Date.MinValue
+    Public mensagem As String = ""
+    Public valorJurosMes As Double = 0
+    Public valorJurosAno As Double = 0
+    Public valorTotalApagar As Double = 0
+    Public valorTotalJuros As Double = 0
+    Public valorTotalAJuros As Double = 0
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+        mensagem = ""
+    End Sub
+
+    Private Sub valorCriterio_TextChanged(sender As Object, e As EventArgs) Handles valorCriterio.TextChanged
+        Try
+            valorCredito = Double.Parse(valorCriterio.Text)
+        Catch ex As Exception
+            mensagem = "Recusado: Valor do crédito preenchido incorretamente."
+        End Try
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            parcelas = Integer.Parse(TextBox1.Text)
+        Catch ex As Exception
+            mensagem = "Recusado: Parcela preenchida incorretamente"
+        End Try
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles primeiraparcela.ValueChanged
+
+        dataVencimento = primeiraparcela.Value
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ret As Object = mensagem
+
+        validacao()
+
+        If mensagem = "" Then
+            validacaoJuros()
+        End If
+
+        TextBox2_TextChanged(ret, e)
+    End Sub
+    Public Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+        If mensagem <> "" Then
+            TextBox2.Text = mensagem
+        Else
+            TextBox2.Text = "Aprovado: Valor Total Juros: R$ " & Double.Parse(valorTotalJuros).ToString("#,##0.00") & ", valor Total com Juros: R$ " & Double.Parse(valorTotalApagar).ToString("#,##0.00")
+        End If
+
+        mensagem = ""
+
+    End Sub
+    Private Sub validacao()
+        Dim valorMaximo As Double = 1000000
+        Dim valorMinimoJur As Double = 15000
+        Dim parcMin As Integer = 5
+        Dim parcMax As Integer = 72
+        If mensagem = "" Then
+            'Valor do crédito
+            If valorCredito > 0 Then
+                If valorCredito > valorMaximo Then
+                    mensagem = "Recusado: O valor máximo a ser liberado para qualquer tipo de empréstimo é de R$ 1.000.000,00"
+                End If
+            Else
+                mensagem = "Recusado: Valor do crédito não preenchido"
+            End If
+
+            'Tipo de Crédito
+            If tipoCredito = "Jurídico" AndAlso valorCredito < valorMinimoJur AndAlso mensagem = "" Then
+                mensagem = "Recusado: Para o crédito de pessoa jurídica, o valor mínimo a ser liberado é de R$ 15.000,00"
+            End If
+
+            'Parcelas
+            If parcelas < parcMin AndAlso mensagem = "" Then
+                mensagem = "Recusado: A quantidade de parcelas mínima é de 5x"
+            ElseIf parcelas > parcMax AndAlso mensagem = "" Then
+                mensagem = "Recusado: A quantidade de parcelas máximas é de 72x"
+            End If
+
+            'Data de vencimento:
+            Dim qtdDias As Integer = CInt(DateDiff(DateInterval.Day, Now.Date, dataVencimento.Date))
+
+            If qtdDias < 15 AndAlso mensagem = "" Then
+                Dim data As Date = DateAdd(DateInterval.Day, 15, Now.Date)
+                mensagem = "Recusado: A data do primeiro vencimento deve ser a partir do dia " & data.ToString("dd/MM/yyyy")
+            ElseIf qtdDias > 40 AndAlso mensagem = "" Then
+                Dim data As Date = DateAdd(DateInterval.Day, 40, Now.Date)
+                mensagem = "Recusado: A data do primeiro vencimento deve ser até o dia " & data.ToString("dd/MM/yyyy")
+            End If
+        End If
+    End Sub
+
+    Private Sub validacaoJuros()
+        Dim CreditoDireto As Integer = 2 '- Taxa de 2% ao mês
+        Dim CreditoConsignado As Integer = 1 '- Taxa de 1% ao mês
+        Dim CreditoPessoaJuridica As Integer = 5 '- Taxa de 5% ao mês
+        Dim CreditoPessoaFisica As Integer = 3 '- Taxa de 3% ao mês
+        Dim CreditoImobiliário As Integer = 9 '- Taxa de 9% ao ano
+        Dim paarc As New List(Of Integer)
+        Dim parc As Integer = 0
+        Dim cont As Integer = 0
+        Dim temvalor As Double = False
+
+        valorJurosMes = 0
+        valorJurosAno = 0
+        valorTotalApagar = 0
+        valorTotalJuros = 0
+        valorTotalAJuros = 0
+
+        If mensagem = "" Then
+            valorJurosMes += (valorCredito * CreditoDireto) / 100
+            valorJurosMes += (valorCredito * CreditoConsignado) / 100
+            If tipoCredito = "Jurídico" Then
+                valorJurosMes += (valorCredito * CreditoPessoaJuridica) / 100
+            Else
+                valorJurosMes += (valorCredito * CreditoPessoaFisica) / 100
+            End If
+
+            valorJurosAno = (valorCredito * CreditoImobiliário) / 100
+
+
+            'Calculo do valor total
+            valorTotalApagar = valorCredito + (valorJurosMes * parcelas)
+
+            While parc < parcelas
+                parc += 1
+                paarc.Add(parc)
+            End While
+
+            For i As Integer = 0 To paarc.Count - 1
+                cont += 1
+                If cont = 12 Then
+                    cont = 0
+                    valorTotalApagar += valorJurosAno
+                    temvalor = True
+                    valorTotalAJuros += valorJurosAno
+                End If
+            Next
+
+            If Not temvalor Then
+                valorTotalApagar += valorJurosAno
+                valorTotalAJuros += valorJurosAno
+            End If
+
+            valorTotalJuros = (valorJurosMes * parcelas)
+            valorTotalJuros += valorTotalAJuros
+        End If
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Try
+            tipoCredito = ComboBox1.SelectedItem
+
+        Catch ex As Exception
+            mensagem = "Recusado: Preencher o tipo de crédito."
+        End Try
+    End Sub
+End Class
